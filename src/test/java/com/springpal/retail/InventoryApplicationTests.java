4fc616fefc3ee86d45a040aad42abdd62a7dd625
Pal-Raks <pallavirakshit88@gmail.com>
2024-09-22 14:58:07 +0530

    Skaleton project

---
+++ src/test/java/com/springpal/retail/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springpal.retail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
